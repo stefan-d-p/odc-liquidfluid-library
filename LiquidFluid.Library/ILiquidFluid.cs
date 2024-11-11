fc80f1f227800eb17706e2a08bd5fd44963c8e1e
Stefan Weber <stefan.weber@spatium.one>
2024-11-11 18:35:13 +0100

    Updated Fluid.Core 2.12.0 Readme added

--- LiquidFluid.Library/ILiquidFluid.cs
+++ LiquidFluid.Library/ILiquidFluid.cs
@@ -5,7 +5,7 @@ namespace Without.Systems.LiquidFluid
     [OSInterface(
         Name = "LiquidFluid",
         Description = "Fluid Template engine is based on the Liquid Template Language created by Shopify. Fluid is a native C# open-source implementation of the Liquid Template Language developed by Sebastien Ros",
-        IconResourceName = "Without.Systems.LiquidFluid.Resources.FluidCore.png")]
+        IconResourceName = "Without.Systems.LiquidFluid.Resources.Liquid.png")]
     public interface ILiquidFluid
     {
         [OSAction(
@@ -13,7 +13,7 @@ namespace Without.Systems.LiquidFluid
             ReturnType = OSDataType.Text,
             ReturnName = "result",
             ReturnDescription = "Rendered result",
-            IconResourceName = "Without.Systems.LiquidFluid.Resources.FluidCore.png")]
+            IconResourceName = "Without.Systems.LiquidFluid.Resources.Liquid.png")]
         string RenderTemplate(
             [OSParameter(
                 Description = "Liquid template",
--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# ODC External Logic Library
+# Liquid Fluid
 
-Documentation....
+This utility library for OutSystems Developer Cloud wraps the Fluid.Core library. Fluid.Core is a templating engine based
+on the [Liquid Template Language](https://shopify.github.io/liquid/).
+
+## RenderTemplate
+
+Input
+
+* `template` - The liquid template
+* `data` - JSON document with the data to merge with the template
+
+Output
+
+* `result` - Rendered result
